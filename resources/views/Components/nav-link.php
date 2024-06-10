0ddc9472f127df55c30cfcd858cbbf2aed16601f
Tristan <73254843+THG20203@users.noreply.github.com>
2024-06-11 00:08:30 +0100

    going through nav link attributes and slot passing down to smallest denomination of file in components

--- resources/views/Components/nav-link.php
+++ resources/views/Components/nav-link.php
@@ -1,1 +1,3 @@
-<a {{ $attributes }}>Home</a>
+<!-- Slot technique to pass down information -->
+<!-- All laravel blade components also have access to an attributes object -->
+<a {{ $attributes }}>{{ $slot }}</a>
