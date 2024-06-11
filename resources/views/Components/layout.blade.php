e6d9c33ed3db2488e608e49d1ad6fba1d0963edd
Tristan <73254843+THG20203@users.noreply.github.com>
2024-06-11 10:21:21 +0100

    initial request method and is function work

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -18,7 +18,12 @@
             </div>
             <div class="hidden md:block">
               <div class="ml-10 flex items-baseline space-x-4">
-                <a href="/" class="{{ false ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white" }} rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
+
+                <!-- Laravel out of the box include a request() helper function. You can call it to obtain information about the current 
+                request. One of the methods on the request() object is is(). is() allows us to pass a regular expression, a pattern - 
+                so if is("/") if the is method has / as a parameter (home page) then apply the active styling -->
+
+                <a href="/" class="{{ request()->is("/") ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white" }} rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
                 <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
                 <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
               </div>
