1a25da5b6e084574d3ebae56bcc2121963deed4f
Tristan <73254843+THG20203@users.noreply.github.com>
2024-06-11 11:51:25 +0100

    sorting out the blade directives for conditional logic

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -10,18 +10,17 @@ so I've added a dynamic check in for it. Page if true and false if not true. -->
 <!-- if you do not provide a prop you can assume its type a, an anchor tag. -->
 @props(["active" => false, "type" => "a"])
 
-<!-- For type check logic should split it into two tags? Use traditional php tags -> if type is a display, then if type = button display a 
-button. -->
-<?php if ($type === "a") : ?>
+<!-- going to use  blade directives for conditional logic instead of php tags -->
+@if($type === "a")
 <a class="{{ $active ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white" }} rounded-md px-3 py-2 text-sm font-medium" aria-current="{{ $active ? "page" : "false" }}" {{ $attributes }}> 
     <!-- slot to inside content into from the layout regarding the link title -->
     {{ $slot }} 
 </a>
-<!-- php else condition for button element -->
-<?php else : ?>
+
+@else
 <button  class="{{ $active ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white" }} rounded-md px-3 py-2 text-sm font-medium" aria-current="{{ $active ? "page" : "false" }}" {{ $attributes }}> 
     <!-- slot to inside content into from the layout regarding the link title -->
     {{ $slot }} 
 </button>
-<!-- php close the conditional -->
-<?php endif ?>
+
+@endif
