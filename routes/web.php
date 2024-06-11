62020af19ced3b95b92a1dd15fbf0d952f567d4d
Tristan <73254843+THG20203@users.noreply.github.com>
2024-06-11 16:44:41 +0100

    implementing class JOb

--- routes/web.php
+++ routes/web.php
@@ -3,38 +3,40 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Arr;
 
-$jobs = [
-    [
-        "id" => 1,
-        "title" => "Director",
-        "salary" => "$50,000"
-    ],
-    [
-        "id" => 2,
-        "title" => "Programmer",
-        "salary" => "$10,000"
-    ],
-    [
-        "id" => 3,
-        "title" => "Teacher",
-        "salary" => "$40,000"
-    ]
-];
+class Job
+{
+    public static function all(): array
+    {
+        return [
+            [
+                "id" => 1,
+                "title" => "Director",
+                "salary" => "$50,000"
+            ],
+            [
+                "id" => 2,
+                "title" => "Programmer",
+                "salary" => "$10,000"
+            ],
+            [
+                "id" => 3,
+                "title" => "Teacher",
+                "salary" => "$40,000"
+            ]
+        ];
+    }
+}
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function () use ($jobs) {
-    /* view('jobs', ["job" => $jobs]) means that the function is returning 
-    a view named jobs. This view will receive data in the form of an array 
-    where the key is "jobs" and the value is whatever is stored in $jobs. This 
-    allows the jobs view to access the $jobs array data. */
-    return view('jobs', ["jobs" => $jobs]);
+Route::get('/jobs', function () {
+    return view('jobs', ["jobs" => Job::all()]);
 });
 
-Route::get('/jobs/{id}', function ($id) use ($jobs) {
-    $job = Arr::first($jobs, fn ($job) => $job["id"] == $id);
+Route::get('/jobs/{id}', function ($id) {
+    $job = Arr::first(Job::all(), fn ($job) => $job["id"] == $id);
     return view('job', ['job' => $job]);
 });
 
