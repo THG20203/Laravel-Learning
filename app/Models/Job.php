ef55695c4f44980d23ef09d4adf5a2d422fa4379
Tristan <73254843+THG20203@users.noreply.github.com>
2024-06-12 14:48:58 +0100

    clearing out job model for eloquent function to be written

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,47 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job
+class Job extends Model
 {
-    /* return type of array */
-    public static function all(): array
-    {
-        return [
-            [
-                "id" => 1,
-                "title" => "Director",
-                "salary" => "$50,000"
-            ],
-            [
-                "id" => 2,
-                "title" => "Programmer",
-                "salary" => "$10,000"
-            ],
-            [
-                "id" => 3,
-                "title" => "Teacher",
-                "salary" => "$40,000"
-            ]
-        ];
-    }
-    /* new method - call it find. Want to find a specific job with an id (and that id should be an integar). */
-    public static function find(int $id): array
-    {
-        /* we're already in job class above so could just say static::all */
-        $job = Arr::first(static::all(), fn ($job) => $job["id"] == $id);
-
-        if (!$job) {
-            abort(404);
-        }
-
-        return $job;
-    }
-
-    /* Model is a key term, comes from the MVC arcitecture. Model, View, Controller.
-    MVC = system for applications. Its how each piece of puzzle should communicate. 
-    
-    Models - Represent data persistence, business logic tier of application
-    Views - Presentation layer
-    Controller - Defining a route then have a function that handles that route? */
 }
+
+/* Model is a key term, comes from the MVC arcitecture. Model, View, Controller.
+MVC = system for applications. Its how each piece of puzzle should communicate. 
+    
+Models - Represent data persistence, business logic tier of application
+Views - Presentation layer
+Controller - Defining a route then have a function that handles that route? */
