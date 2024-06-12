69a1fbb5707b11d1662a75345f6a74115792cd17
Tristan <73254843+THG20203@users.noreply.github.com>
2024-06-12 17:23:24 +0100

    setting up factory data with job listings table

---
+++ database/factories/JobFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
+ */
+class JobFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "title" => fake()->jobTitle(),
+            // in situations where it doesn't matter -> can hard code a value like for salary
+            "salary" => "$50,000 USD"
+        ];
+    }
+}
