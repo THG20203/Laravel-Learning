4dff7f12cba89b5105e88acd417bbda0b0d34a62
Tristan <73254843+THG20203@users.noreply.github.com>
2024-06-13 14:46:02 +0100

    further card styling

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -4,11 +4,11 @@
     <!-- Heading Slot -->
     <x-slot:heading>Jobs Page</x-slot:heading>
 
-    <div>
+    <div class="space-y-4">
         <!-- Going to do a foreach with a blade directive -->
         @foreach ($jobs as $job)
                 <!-- <a> tag to take me to a new route. Identifier in place for 'said route' -->
-                <a href="/jobs/{{ $job['id'] }}" class="text-blue-500 hover:underline block px-4 py-6 border border-gray-200">
+                <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
                     <strong>{{ $job['title']}}:</strong> Pays {{ $job['salary'] }} per year.
                 </a>
         <!-- End of the foreach block -->
