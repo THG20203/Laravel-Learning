662d655942c50a8b83a1bc0f2bb4b8253245018c
Tristan <73254843+THG20203@users.noreply.github.com>
2024-06-13 12:21:47 +0100

    create tags table sorted

--- database/migrations/2024_06_13_104604_create_tags_table.php
+++ database/migrations/2024_06_13_104604_create_tags_table.php
@@ -21,10 +21,20 @@ return new class extends Migration
         This is our pivot table. In here, could store the tag id as well as the job id. */
         Schema::create('job_tag', function (Blueprint $table) {
             $table->id();
-            /* handle our job id column */
-            $table->foreignIdFor(\App\Models\Job::class);
+
+            /* handle our job id column - changed it to a job listing id so its not 
+            interpreted as the job table out of the box with laravel. */
+
+            /* Also on job listing id, add a call to contrained and then a second one to cascade
+            on delete */
+
+            /* constrained() create a constraint */
+            /* cascadeOnDelete() if that referencing record happens to be deleted, want to cascade
+            and delete pivot record as well. (cascade meaning lead on/ continue onto this process) */
+
+            $table->foreignIdFor(\App\Models\Job::class, "job_listing_id")->constrained()->cascadeOnDelete();
             /* handle our tag id column */
-            $table->foreignIdFor(\App\Models\Tag::class);
+            $table->foreignIdFor(\App\Models\Tag::class)->constrained()->cascadeOnDelete();
             /* when create a new record in pivot table, would I like to track the time stamp 
             for when that happened */
             $table->timestamps();
