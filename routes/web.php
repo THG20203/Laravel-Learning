dff08c515d55f4ced80b60a604c8ab7b44737833
Tristan <73254843+THG20203@users.noreply.github.com>
2024-06-13 15:59:32 +0100

    importing the model to make preventlazyloading method

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,19 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    return view('jobs', ["jobs" => Job::all()]);
+    // Eager load jobs with the employer for each one
+
+    /* Job::with('employer'): This line tells Eloquent to prepare a query that will retrieve Job 
+    models and, at the same time, load the associated employer relationships. The employer is 
+    expected to be a defined relationship (like belongsTo or hasOne) in the Job model class. */
+
+    /* ->get(): This method executes the query and retrieves the results. It fetches all records 
+    from the jobs table and their related employer data in one go, rather than querying for the 
+    employer each time a job is accessed, which would happen in lazy loading. */
+
+    $jobs = Job::with('employer')->get();
+
+    return view('jobs', ["jobs" => $jobs]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
