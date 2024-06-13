92dc9bb86d30330c8025f608d20cffe18949eea8
Tristan <73254843+THG20203@users.noreply.github.com>
2024-06-13 10:34:03 +0100

    employer and job factory sorted

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -18,6 +18,10 @@ class JobFactory extends Factory
     {
         return [
             "title" => fake()->jobTitle(),
+            /* need to update JobFactory to include employer_id. Employer factory - whenever you generate a job, 
+            I would also like you to generate corrresponding relationships with employer factory. So need to generate
+            that, persist it in the database then use that as corresponding employer id. */
+            "employer_id" => Employer::factory(),
             // in situations where it doesn't matter -> can hard code a value like for salary
             "salary" => "$50,000 USD"
         ];
