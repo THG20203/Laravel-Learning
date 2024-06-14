6fe154b6fcd03a35caeefacf17f776d3caf7beab
Tristan <73254843+THG20203@users.noreply.github.com>
2024-06-14 09:48:38 +0100

    restructuring the jobs files

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,18 @@ Route::get('/jobs', function () {
     /* for first argument can specify how many records do we want to show per page */
     $jobs = Job::with('employer')->cursorPaginate(3);
 
-    return view('jobs', ["jobs" => $jobs]);
+    return view('jobs.index', ["jobs" => $jobs]);
 });
 
+// Show a page to create a new job
+Route::get("/jobs/create", function () {
+    return view('jobs.create');
+});
+
+// Wildcard should go close to the bottom of route declarations
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
-    return view('job', ['job' => $job]);
+    return view('job.show', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
