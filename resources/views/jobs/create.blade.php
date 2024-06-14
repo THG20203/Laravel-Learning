4c9f8ab8919634acec7689a0dbad5a08faa20892
Tristan <73254843+THG20203@users.noreply.github.com>
2024-06-14 11:21:59 +0100

    form styling complete ready for functionality

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -20,8 +20,8 @@
 <form>
     <div class="space-y-12">
       <div class="border-b border-gray-900/10 pb-12">
-        <h2 class="text-base font-semibold leading-7 text-gray-900">Profile</h2>
-        <p class="mt-1 text-sm leading-6 text-gray-600">This information will be displayed publicly so be careful what you share.</p>
+        <h2 class="text-base font-semibold leading-7 text-gray-900">Create a new job</h2>
+        <p class="mt-1 text-sm leading-6 text-gray-600">We just need a handful of details from you.</p>
   
         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
           <!-- Title of Job -->
@@ -29,16 +29,16 @@
             <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
             <div class="mt-2">
               <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                <input type="text" name="title" id="title" autocomplete="title" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Example Job">
+                <input type="text" name="title" id="title" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Example Job">
               </div>
             </div>
           </div>
-            <!-- Title of Job -->
+            <!-- Salary of Job -->
             <div class="sm:col-span-4">
-                <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
+                <label for="salary" class="block text-sm font-medium leading-6 text-gray-900">Salary</label>
                 <div class="mt-2">
                   <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                    <input type="text" name="title" id="title" autocomplete="title" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Example Job">
+                    <input type="text" name="salary" id="salary" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="$50,000 per year">
                   </div>
                 </div>
             </div>
