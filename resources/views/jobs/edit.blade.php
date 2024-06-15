34eb98fc5546be8d3a09024bfba2169a3f9e7777
Tristan <73254843+THG20203@users.noreply.github.com>
2024-06-15 13:06:13 +0100

    added value into edit title and salary so there is data to change

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -16,7 +16,7 @@
             <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
             <div class="mt-2">
               <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                <input type="text" name="title" id="title" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Example Job" required>
+                <input type="text" name="title" id="title" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Example Job" value="{{ $job->title}}" required>
               </div>
                  <!-- New error blade directive. reference the attribute name (title). If we have a validation error for the title
                   only on thatt condition should we procede -->
@@ -31,7 +31,7 @@
                 <label for="salary" class="block text-sm font-medium leading-6 text-gray-900">Salary</label>
                 <div class="mt-2">
                   <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                    <input type="text" name="salary" id="salary" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="$50,000 per year" required>
+                    <input type="text" name="salary" id="salary" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="$50,000 per year" value="{{ $job->salary}}" required>
                   </div>
                   @error('salary')
                   <!-- Message varialbe is only available in error directive -->
--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -2,11 +2,9 @@
     <x-slot:heading>
         Job
     </x-slot:heading>
-    <h2 class="font-bold text-lg">{{ $job['title'] }}</h2>
-    <p>This job pays {{ $job['salary'] }} per year.</p>
+    <h2 class="font-bold text-lg">{{ $job->title }}</h2>
+    <p>This job pays {{ $job->salary }} per year.</p>
     <p class="mt-6">
-        <!-- Notice how {{ $job-> id}} is accessing them as properties, but above I'm using 
-        array. Its because before I didn't know eloquent so will swap them over-->
         <x-button href="/jobs/{{ $job-> id}}/edit">Edit Job</x-button>
     </p>
 </x-layout>
