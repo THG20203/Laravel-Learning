954dba33864d564b009a49d6c3070c9ea79b8701
Tristan <73254843+THG20203@users.noreply.github.com>
2024-06-17 17:06:48 +0100

    job controller sorted

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,16 @@ use Illuminate\Support\Facades\Route;
 // Shorthand for displaying view for home 
 Route::view('/', 'home');
 
-/* Job routes together below */
-Route::get('/jobs', [JobController::class, 'index']);
-Route::get("/jobs/create", [JobController::class, 'create']);
-Route::get('/jobs/{job}', [JobController::class], 'show');
-Route::post('/jobs', [JobController::class], 'store');
-Route::get('/jobs/{job}/edit', [JobController::class], 'edit');
-Route::patch("/jobs/{job}", [JobController::class], 'update');
-Route::delete("/jobs/{job}", [JobController::class], 'destroy');
+/* Job route group where all assume Job Controller */
+Route::controller(JobController::class)->group(function () {
+    Route::get('/jobs', 'index');
+    Route::get("/jobs/create", 'create');
+    Route::get('/jobs/{job}', 'show');
+    Route::post('/jobs', 'store');
+    Route::get('/jobs/{job}/edit', 'edit');
+    Route::patch("/jobs/{job}", 'update');
+    Route::delete("/jobs/{job}", 'destroy');
+});
 
 // Shorthand for displaying view for contact
 Route::view('/contact', 'contact');
