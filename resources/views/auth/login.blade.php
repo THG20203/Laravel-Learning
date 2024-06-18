a9607c946f8014bafa07003327a150898b749af8
Tristan <73254843+THG20203@users.noreply.github.com>
2024-06-18 14:03:35 +0100

    email and password only on the login auth view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,14 +3,36 @@
         Login
     </x-slot:heading>
 <!-- Form functionality -->
-<form method="POST" action="/jobs">
+<form method="POST" action="">
     <!-- blade directive at the top of the file -->
     @csrf
     
     <div class="space-y-12">
       <div class="border-b border-gray-900/10 pb-12">
         <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-          
+            <!-- Email -->
+            <x-form-field>
+                <!-- label section -->
+                <x-form-label for="email">Email</x-form-label>
+                <!-- input and error section -->
+                <div class="mt-2">
+                    <!-- On email overwriting the type to make it email -->
+                    <x-form-input name="email" id="email" type="email" required />
+                    <x-form-error name="email" />
+                </div>
+            </x-form-field>
+
+            <!-- Password -->
+            <x-form-field>
+                <!-- label section -->
+                <x-form-label for="password">Password</x-form-label>
+                <!-- input and error section -->
+                <div class="mt-2">
+                    <!-- On email overwriting the type to make it email -->
+                    <x-form-input name="password" id="password" type="password" required />
+                    <x-form-error name="password" />
+                </div>
+            </x-form-field>
         </div>
       </div>
     </div>
