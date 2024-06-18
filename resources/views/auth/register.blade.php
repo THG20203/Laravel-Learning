5c240f18a8070454b75dd2542610bf1c7de7bf24
Tristan <73254843+THG20203@users.noreply.github.com>
2024-06-18 14:22:11 +0100

    changing register blade file

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,82 +1,70 @@
 <x-layout>
-    <x-slot:heading>
-        Register
-    </x-slot:heading>
-<!-- Form functionality -->
-<form method="POST" action="/jobs">
-    <!-- blade directive at the top of the file -->
-    @csrf
-    
-    <div class="space-y-12">
-      <div class="border-b border-gray-900/10 pb-12">
-        <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-          <!-- First Name -->
-          <x-form-field>
-            <!-- label section -->
-            <x-form-label for="first_name">First Name</x-form-label>
-            <!-- input and error section --> 
-            <div class="mt-2">
-                <x-form-input name="first_name" id="first_name" required />
-                <x-form-error name="first_name" />
-            </div>
-          </x-form-field>
+  <x-slot:heading>
+      Register
+  </x-slot:heading>
 
-          <!-- Last Name -->
-          <x-form-field>
-            <!-- label section -->
-            <x-form-label for="last_name">Last Name</x-form-label>
-            <!-- input and error section --> 
-            <div class="mt-2">
-                <x-form-input name="last_name" id="last_name" required />
-                <x-form-error name="last_name" />
-            </div>
-          </x-form-field>
+  <form method="POST" action="/register">
+      @csrf
 
-          <!-- Email -->
-          <x-form-field>
-            <!-- label section -->
-            <x-form-label for="email">Email</x-form-label>
-            <!-- input and error section -->
-            <div class="mt-2">
-                <!-- On email overwriting the type to make it email -->
-                <x-form-input name="email" id="email" type="email" required />
-                <x-form-error name="email" />
-            </div>
-          </x-form-field>
+      <div class="space-y-12">
+          <div class="border-b border-gray-900/10 pb-12">
+              <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                  <x-form-field>
+                      <x-form-label for="first_name">First Name</x-form-label>
 
-          <!-- Password -->
-          <x-form-field>
-            <!-- label section -->
-            <x-form-label for="password">Password</x-form-label>
-            <!-- input and error section -->
-            <div class="mt-2">
-                <!-- On email overwriting the type to make it email -->
-                <x-form-input name="password" id="password" type="password" required />
-                <x-form-error name="password" />
-            </div>
-          </x-form-field>
+                      <div class="mt-2">
+                          <x-form-input name="first_name" id="first_name" required />
 
-          <!-- Password Confirmation -->
-          <x-form-field>
-            <!-- label section -->
-            <x-form-label for="password_confirmation">Confirm Password</x-form-label>
-            <!-- input and error section -->
-            <div class="mt-2">
-                <!-- On email overwriting the type to make it email -->
-                <x-form-input name="password_confirmation" id="password_confirmation" type="password_confirmation" required />
-                <x-form-error name="password_confirmation" />
-            </div>
-          </x-form-field>
+                          <x-form-error name="first_name" />
+                      </div>
+                  </x-form-field>
 
-        </div>
+                  <x-form-field>
+                      <x-form-label for="last_name">Last Name</x-form-label>
+
+                      <div class="mt-2">
+                          <x-form-input name="last_name" id="last_name" required />
+
+                          <x-form-error name="last_name" />
+                      </div>
+                  </x-form-field>
+
+                  <x-form-field>
+                      <x-form-label for="email">Email</x-form-label>
+
+                      <div class="mt-2">
+                          <x-form-input name="email" id="email" type="email" required />
+
+                          <x-form-error name="email" />
+                      </div>
+                  </x-form-field>
+
+                  <x-form-field>
+                      <x-form-label for="password">Password</x-form-label>
+
+                      <div class="mt-2">
+                          <x-form-input name="password" id="password" type="password" required />
+
+                          <x-form-error name="password" />
+                      </div>
+                  </x-form-field>
+
+                  <x-form-field>
+                      <x-form-label for="password_confirmation">Confirm Password</x-form-label>
+
+                      <div class="mt-2">
+                          <x-form-input name="password_confirmation" id="password_confirmation" type="password_confirmation" required />
+
+                          <x-form-error name="password_confirmation" />
+                      </div>
+                  </x-form-field>
+              </div>
+          </div>
       </div>
-    </div>
-  
-    <div class="mt-6 flex items-center justify-end gap-x-6">
-      <!-- This can be a simple link takes you back to the home page -->
-      <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
-      <x-form-button>Register</x-form-button>
-    </div>
+
+      <div class="mt-6 flex items-center justify-end gap-x-6">
+          <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
+          <x-form-button>Register</x-form-button>
+      </div>
   </form>
-  
 </x-layout>
