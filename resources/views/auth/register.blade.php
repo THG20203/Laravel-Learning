196ed832dc2c66e3151f39ce5606524c51a65287
Tristan <73254843+THG20203@users.noreply.github.com>
2024-06-18 12:26:35 +0100

    first name and last name inputs sorted

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,12 +11,22 @@
       <div class="border-b border-gray-900/10 pb-12">
         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
           <x-form-field>
-            <!-- Register Form field label -->
+            <!-- label section -->
             <x-form-label for="first_name">First Name</x-form-label>
-            <!-- Register Form Field input section --> 
+            <!-- input and error section --> 
             <div class="mt-2">
                 <x-form-input name="first_name" id="first_name" />
                 <x-form-error name="first_name" />
+            </div>
+          </x-form-field>
+
+          <x-form-field>
+            <!-- label section -->
+            <x-form-label for="first_name">Last Name</x-form-label>
+            <!-- input and error section --> 
+            <div class="mt-2">
+                <x-form-input name="last_name" id="last_name" />
+                <x-form-error name="last_name" />
             </div>
           </x-form-field>
         </div>
