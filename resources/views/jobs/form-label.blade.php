b5d20bf336a0f8384b05eda53c666b43af6216c8
Tristan <73254843+THG20203@users.noreply.github.com>
2024-06-18 09:32:23 +0100

    sorting out attributes and explaining them with comments

--- resources/views/jobs/form-label.blade.php
+++ resources/views/jobs/form-label.blade.php
@@ -1,1 +1,7 @@
-<label class="block text-sm font-medium leading-6 text-gray-900">{{ $slot }}</label>
+<!-- Dynamic Attribute Merging: The merge method allows for dynamic addition and merging of attributes, ensuring the 
+component is flexible and can be customized as needed. -->
+
+<!-- Default Styling: By providing a default class attribute, the component ensures a consistent base styling that 
+can be extended. -->
+
+<label {{ $attributes->merge(['class' => "block text-sm font-medium leading-6 text-gray-900"]) }}>{{ $slot }}</label>
