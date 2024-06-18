c087a2c52e2323e45db30f7efdb76c490541f815
Tristan <73254843+THG20203@users.noreply.github.com>
2024-06-18 09:36:45 +0100

    adding label into create file using components layout

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -16,7 +16,7 @@
           
           <!-- Title of Job Form Field -->
           <div class="sm:col-span-4">
-            <x-form-label />
+            <x-form-label for="title">Title<x-form-label>
             <div class="mt-2">
               <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                 <input type="text" name="title" id="title" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Example Job" required>
