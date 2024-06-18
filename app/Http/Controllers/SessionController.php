8dd2e10b5e860c2b4511c13aa31dfbb3c3021c23
Tristan <73254843+THG20203@users.noreply.github.com>
2024-06-18 22:06:13 +0100

    destroy with logout completed

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SessionController extends Controller
 {
@@ -12,6 +13,15 @@ class SessionController extends Controller
     }
     public function store()
     {
-        dd(request()->all());
+        // validate user
+
+        // attempt to login user
+    }
+    public function destroy()
+    {
+        /* with logout, don't have to specify a user cause it assumes the current user. */
+        Auth::logout();
+        /* after logout a user, redirect them to the home page */
+        return redirect('/');
     }
 }
