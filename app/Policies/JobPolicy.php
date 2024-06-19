1584bb92d64e715f6e3f13730d63a760e4848c38
Tristan <73254843+THG20203@users.noreply.github.com>
2024-06-19 17:10:06 +0100

    explaining boolean value

--- app/Policies/JobPolicy.php
+++ app/Policies/JobPolicy.php
@@ -11,10 +11,11 @@ class JobPolicy
     /**
      * Determine whether the user can edit any models. This accepts the user and the job.
      * So policies are almost like a version of a gate, sort of the same thing, just an interface for it.
+     * bool because its going to return a boolean value like Gate definition so I'm adding bool return type
      */
-    public function edit(User $user, Job $job)
+    public function edit(User $user, Job $job): bool
     {
-        //
+        return $job->employer->user->is($user);
     }
 }
 
