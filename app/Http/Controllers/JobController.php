00ecb8b43eee9c630578555036dff22b990142a4
Tristan <73254843+THG20203@users.noreply.github.com>
2024-06-19 11:51:49 +0100

    removing logic that wasn't needed anymore, rellocating comment explanations

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -48,31 +48,6 @@ class JobController extends Controller
     }
     public function edit(Job $job)
     {
-        // GATE AUTHORISED CONTENT
-        /* Use Gate facade - conditionally allow entry if you meet certain criteria, if you are
-        authorised that gate will open, but if you are unauthorised the gate is locked. */
-        /* First: Given Gate the name edit-job. */
-        /* Second: PAss a function that will accept the signed in user, as well as the job we are authorising. */
-        Gate::define('edit-job', function (User $user, Job $job) {
-            /* within here we should return a boolean, indicates whether or not user is authorised to edit that 
-            job. */
-            /* return if the employer user IS the currently signed in user. Simpler to just to return this as the true value
-            in the Gate boolean */
-            return $job->employer->user->is($user);
-        });
-
-        /* Explanation of the logic above (for understanding) - taking logic and extracting it into its own gate. The gate
-        is called edit-job and the logic for whether you can edit a job is look at the user responsible for the job 
-        (the user-> section) and see if that 'user' is the same as the user that is currently signed in, (which we get from 
-        (User $user) */
-
-        // IF NOT SIGNED IN
-        /* to edit job, should be signed in so going to check if you're a guest, if so redirect you 
-        to the login page. */
-        if (Auth::guest()) {
-            return redirect('/login');
-        }
-
         /* now can reference that logic to authorie user - providing the name 'edit-job' and then the $job (job variable) we 
         are referring to. */
         /* The authorize method will run the logic associated with the name I reference (i.e. 'edit-job'). If it fails/ returns
