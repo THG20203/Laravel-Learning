f7b2cee5d419f9fa12ac742ba3b52489c513af24
Tristan <73254843+THG20203@users.noreply.github.com>
2024-06-19 13:59:30 +0100

    can directive best for edit button being display or not

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -1,10 +1,19 @@
 <x-layout>
+
     <x-slot:heading>
         Job
     </x-slot:heading>
+
     <h2 class="font-bold text-lg">{{ $job->title }}</h2>
+
     <p>This job pays {{ $job->salary }} per year.</p>
+
+    <!-- if you can edit the job, (with 'edit-job' and passing in the particular job with the $job variable) 
+    only should we show this mark up -->
+    @can('edit-job', $job) 
     <p class="mt-6">
         <x-button href="/jobs/{{ $job-> id}}/edit">Edit Job</x-button>
     </p>
+    @endcan
+
 </x-layout>
