bf2a3a005e2fb44af8edc6d7cfe197c028c32425
Tristan <73254843+THG20203@users.noreply.github.com>
2024-06-20 13:16:45 +0100

    creating a mail listing

--- resources/views/mail/job-posted.blade.php
+++ resources/views/mail/job-posted.blade.php
@@ -1,7 +1,9 @@
+<h2>{{ $job->title }}</h2>
+
 <p>Congratulations! Your job is now live on our website.</p>
 
 <p>
     <!-- Keep in mind when the user receives this email, they're not on this website, so I need to provide the
-    full URL, hence {{ url'/jobs/' }} -->
+    full URL, hence {{ url("/jobs/" . $job->id) }} -->
     <a href="{{ url('/jobs/' . $job->id) }}">View Your Job Listing</a>
 </p>
